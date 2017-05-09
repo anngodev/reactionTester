ade8c8c4cf3c645ec10714d3a5c9157f97d79b13
An Ngo <ngoan_230@yahoo.com>
2017-05-09 05:12:04 -0400

    random sizes for shape

--- assets/javascripts/reactionTester.js
+++ assets/javascripts/reactionTester.js
@@ -1,6 +1,21 @@
 var start = new Date().getTime();
 
 function makeShapeAppear() {
+
+	var top = Math.random() * 400;
+
+	var	left = Math.random() * 400;
+
+	var width = Math.random() * 300;
+
+	document.getElementById("shape").style.width = width + "px";
+
+	document.getElementById("shape").style.height = width + "px";
+
+	document.getElementById("shape").style.top = top + "px";
+
+	document.getElementById("shape").style.left = left + "px";
+
 	document.getElementById("shape").style.display = "block";
 	}
 
