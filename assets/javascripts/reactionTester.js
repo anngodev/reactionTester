57e6f2034e51ab067f71284b77bd11722368c3ac
An Ngo <ngoan_230@yahoo.com>
2017-05-09 05:31:32 -0400

    added circles fixed getTime error and styled html

--- assets/javascripts/reactionTester.js
+++ assets/javascripts/reactionTester.js
@@ -1,12 +1,35 @@
 var start = new Date().getTime();
 
+function getRandomColor() {
+
+    var letters = "0123456789ABCDEF".split("");
+
+    var color = "#";
+
+    for (var i = 0; i < 6; i++ ) {
+
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
 function makeShapeAppear() {
 
 	var top = Math.random() * 400;
 
 	var	left = Math.random() * 400;
 
-	var width = Math.random() * 300;
+	var width = (Math.random() * 200) + 100;
+
+	if (Math.random() > 0.5) {
+
+		document.getElementById("shape").style.borderRadius = "50%";
+	} else {
+		document.getElementById("shape").style.borderRadius = "0%"
+	}
+
+	document.getElementById("shape").style.backgroundColor = getRandomColor();
 
 	document.getElementById("shape").style.width = width + "px";
 
@@ -17,6 +40,8 @@ function makeShapeAppear() {
 	document.getElementById("shape").style.left = left + "px";
 
 	document.getElementById("shape").style.display = "block";
+
+	start = new Date().getTime();
 	}
 
 	function appearAfterDelay() {
