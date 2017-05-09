fa6f4ed38fb19370bc2e1b1e0439e7c56f3d3d23
An Ngo <ngoan_230@yahoo.com>
2017-05-09 05:02:54 -0400

    onclick shape with reaction timer

---
+++ assets/javascripts/reactionTester.js
@@ -0,0 +1,26 @@
+var start = new Date().getTime();
+
+function makeShapeAppear() {
+	document.getElementById("shape").style.display = "block";
+	}
+
+	function appearAfterDelay() {
+		setTimeout(makeShapeAppear, Math.random() * 2000);
+
+	}
+
+	appearAfterDelay();
+
+	document.getElementById("shape").onclick = function() {
+		
+		document.getElementById("shape").style.display = "none";
+
+		var end = new Date().getTime();
+
+		var timeTaken = (end - start) / 1000;
+
+		document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+
+		appearAfterDelay();
+
+	}
